7f20d441fd95418a5c5eda40fe1d51d15601e0a9
TooLuckyy <darreljelly@gmail.com>
2026-04-15 00:07:17 -0500

    Addition: add book function

--- backend/addBook.php
+++ backend/addBook.php
@@ -7,16 +7,31 @@ $isbn = "619";
 $genre = "Thriller";
 
 try {
-    $insertSql = "INSERT INTO book(title, author, isbn, genre)
-                  VALUES (:title, :author, :isbn, :genre)";
+    //check if book already exist
+    $checkSql = "SELECT isbn FROM book WHERE isbn = ?";
+    $checkStmt = $pdo->prepare($checkSql);
+    $checkStmt->execute([$isbn]);
+
+    $result = $checkStmt->fetch(PDO::FETCH_ASSOC);
+
+    if($result) {
+        throw new Exception("Book already exist.");
+    }
+
+    //adds book to book table
+    $insertSql = "INSERT INTO book (title, author, isbn, genre) 
+                  VALUES(:title, :author, :isbn, :genre)";
     $insertStmt = $pdo->prepare($insertSql);
     $insertStmt->execute([
-        'title' => $title,
-        'author' => $author,
-        'isbn' => $isbn,
-        'genre' => $genre
-    ]);
+        'title' => $title, 
+        'author' => $author, 
+        'isbn' => $isbn, 
+        'genre' => $genre]);
 
+    $bookId = $pdo->lastInsertId();
+    $copySql = "INSERT INTO bookcopy (bookID, status) VALUES (?, 'available')";
+    $pdo->prepare($copySql)->execute([$bookId]);
+        
     echo "Successfully added " . $title;
 
 } catch (\PDOException $e) {
